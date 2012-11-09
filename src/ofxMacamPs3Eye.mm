e74e4f9e1294485eccdd7fe70faf0a341abca906
Andrew Donaldson <andrew@desto.net>
2012-11-09 13:28:03 +0000

    Second fix for abstract class issue
    Update the type for setPixelFormat (from void to bool) to get it compiling.

--- src/ofxMacamPs3Eye.mm
+++ src/ofxMacamPs3Eye.mm
@@ -159,8 +159,9 @@ float ofxMacamPs3Eye::getRealFrameRate(){
 	return [ofxMacamPs3EyeCast(ps3eye) realFps];
 }
 
-void ofxMacamPs3Eye::setPixelFormat(ofPixelFormat pixelFormat) {
+bool ofxMacamPs3Eye::setPixelFormat(ofPixelFormat pixelFormat) {
 	ofLogVerbose("ofxMacamPs3Eye:: setPixelFormat is not implemented.");
+	return true;
 };
 ofPixelFormat ofxMacamPs3Eye::getPixelFormat() {
 	return OF_PIXELS_RGB;
